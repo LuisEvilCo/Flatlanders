1a38bf5b2be0a457b351e053f3dd464c3055c9d2
Luis Tinajero Muñozcano <ltm150895@gmail.com>
2016-06-28 18:44:38 -0500

    iteration in gems

--- js/app.js
+++ js/app.js
@@ -1,13 +1,24 @@
 (function(){
     var app = angular.module('store',[ ]);
 
-    app.controler('StoreController', function () {
-        
+    app.controller('StoreController', function () {
+        this.products = gems;
     });
 
-    var gem = {
-        name: 'Dodecaheadron',
-        price: 2.95,
-        description: '...'
-    }
+    var gems = [
+        {
+            name: 'Dodecahedron',
+            price: 2.95,
+            description: 'Description for the dodecahedron gem',
+            canPurchase: true,
+            soldOut: false
+        },
+        {
+            name: 'Pentagonal gem',
+            price: 5.95,
+            description: 'Description for the pentagonal gem',
+            canPurchase: true,
+            soldOut: false
+        }
+    ]
 })();
