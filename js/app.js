30067deab422f157dc2e8e15ce0d4403d715e42f
Luis Tinajero Muñozcano <ltm150895@gmail.com>
2016-06-28 17:38:17 -0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+(function(){
+    var app = angular.module('store',[ ]);
+
+    app.controler('StoreController', function () {
+        
+    });
+
+    var gem = {
+        name: 'Dodecaheadron',
+        price: 2.95,
+        description: '...'
+    }
+})();
